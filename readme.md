5dfa55537c6e33080afb90b1c6111eb60679c82b
QiubyZ <lessonphreaker@gmail.com>
2024-10-28 20:12:17 +0800

    first commit

---
+++ readme.md
@@ -0,0 +1,36 @@
+# Acode Kotlin Language Client
+
+This a Experiment Kotlin Language Server on Acode
+[Example Source Code](https://acode.app/plugin/acode.language.client)
+
+## Acode Plugin Requiriment Installed
+
+1. [Acode Language Client](https://acode.app/plugin/acode.language.client) for kotlin Autocomplete
+
+## Setup
+
+### Setup Language Server
+
+- Download _server.zip_ in [kotlin-language-server](https://github.com/fwcd/kotlin-language-server) repository.
+
+- Extract _server.zip_
+
+- copy fullpath binary _kotlin-language-server_
+
+- Goto settings > Edit Settings.json
+  add this json and fullpath binary file of _kotlin-language-server_ on serverPath.
+  or in a simple way you can click the gear button above
+
+  Example:
+
+  ```json
+    "acode.kotlin.client": {
+      "serverPath": "/data/data/com.termux/files/home/server/bin/kotlin-language-server"
+  }
+  ```
+
+![Example](./settings.jpg)
+
+    if you not setting. this will take automatically Default serverPath/Default Settings
+    is neovim Path bin language server
+    */data/data/com.termux/files/home/.local/share/nvim/mason/bin/kotlin-language-server*
---
+++ src/main.js
@@ -0,0 +1,98 @@
+import plugin from "../plugin.json";
+
+let url = acode.require("url");
+// let plugin = {
+//   id: "acode.kotlin.client",
+// };
+//
+class AcodePlugin {
+  async init() {
+    let acodeLanguageClient = acode.require("acode-language-client");
+    if (acodeLanguageClient) {
+      this.setupLangaugeClient(acodeLanguageClient);
+    } else {
+      window.addEventListener("plugin.install", ({ detail }) => {
+        if (detail.name === "acode-language-client") {
+          acodeLanguageClient = acode.require("acode-language-client");
+          this.setupLangaugeClient(acodeLanguageClient);
+        }
+      });
+    }
+  }
+  setupLangaugeClient(acodeLanguageClient) {
+    let socket = (this.socket = acodeLanguageClient.getSocketForCommand(
+      this.settings.serverPath,
+    ));
+    let javaClient = new acodeLanguageClient.LanguageClient({
+      type: "socket",
+      socket,
+    });
+    acodeLanguageClient.registerService("kotlin|kts", javaClient);
+    acode.registerFormatter("Kotlin Language Client", ["kotlin"], () =>
+      acodeLanguageClient.format(),
+    );
+  }
+  get settings() {
+    if (!window.acode) {
+      return this.defaultSettings;
+    }
+    const AppSettings = acode.require("settings");
+    let value = AppSettings.value[plugin.id];
+    if (!value) {
+      value = AppSettings.value[plugin.id] = this.defaultSettings;
+      AppSettings.update();
+    }
+    return value;
+  }
+  get defaultSettings() {
+    return {
+      serverPath:
+        "/data/data/com.termux/files/home/.local/share/nvim/mason/bin/kotlin-language-server",
+    };
+  }
+
+  get settingsObject() {
+    const AppSettings = acode.require("settings");
+    return {
+      list: [
+        {
+          key: "serverPath",
+          text: "Path to Java jdtls server",
+          prompt: "Path to Java jdtls server",
+          promptType: "text",
+          value: this.settings.serverPath,
+        },
+      ],
+      cb: (key, value) => {
+        switch (key) {
+          case "serverPath":
+            if (!value.endsWith("")) {
+              value = value + "/";
+            }
+            break;
+        }
+        AppSettings.value[plugin.id][key] = value;
+        AppSettings.update();
+      },
+    };
+  }
+
+  async destroy() {}
+}
+
+if (window.acode) {
+  const acodePlugin = new AcodePlugin();
+  acode.setPluginInit(
+    plugin.id,
+    async (baseUrl, $page, { cacheFileUrl, cacheFile }) => {
+      if (!baseUrl.endsWith("/")) {
+        baseUrl += "/";
+      }
+      acodePlugin.baseUrl = baseUrl;
+      await acodePlugin.init($page, cacheFile, cacheFileUrl);
+    },
+  );
+  acode.setPluginUnmount(plugin.id, () => {
+    acodePlugin.destroy();
+  });
+}
